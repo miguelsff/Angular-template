91a7c7a531c99a55d02fd4cfdb38393b6ef94a0f
Miguel Concepción <miguelsff@gmail.com>
2021-12-07 09:47:51 -0500

    Adding ngx-translate core loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'angular-template';
+
+  constructor(
+    private primeNGConfig: PrimeNGConfig,
+    private translateService: TranslateService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.translateService.setDefaultLang('es');
+    this.translateService.use("es");
+    this.translateService.get('primeng').subscribe(res => this.primeNGConfig.setTranslation(res));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
 import { AdminModule } from './features/admin/admin.module';
 import { PublicModule } from './features/public/public.module';
 import { AuthModule } from './features/auth/auth.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -20,7 +29,15 @@ import { SharedModule } from './shared/shared.module';
     AdminModule,
     AppRoutingModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
